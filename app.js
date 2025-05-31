63710dfc75eb85c82c3f41d60fea0775b0e25f97
AcertosOnline <acertosonlinejogodobicho@gmail.com>
2025-05-31 12:42:47 -0300

    Update app.js

--- app.js
+++ app.js
@@ -47,15 +47,3 @@ const pwaBanner = `
 
   // Check at load
   showPWABanner();
-
-  // Register Service Worker
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
-      .then(function(registration) {
-        console.log('Service Worker registered with scope:', registration.scope);
-      })
-      .catch(function(error) {
-        console.error('Service Worker registration failed:', error);
-      });
-  }
-});
